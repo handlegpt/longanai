0cd78c541c7d43ae608a4bfbf78cdc411b61f32b
tomi <tomi@tomi-2.local>
2025-07-20 23:05:25 +0900

    Fix Base import in models __init__.py

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,4 +1,8 @@
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
 from .podcast import Podcast
 from .user import User
 
-__all__ = ["Podcast", "User"] 
+__all__ = ["Base", "Podcast", "User"] 
